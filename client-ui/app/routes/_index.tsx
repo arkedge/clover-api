49527a762af0fb75bfd4d978765f1f2ac022798b
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2024-11-07 16:15:28 +0900

    Show the satellite list on the index page

--- client-ui/app/routes/_index.tsx
+++ client-ui/app/routes/_index.tsx
@@ -1,3 +1,33 @@
+import { Card, CardList, Section, SectionCard } from "@blueprintjs/core";
+import { ChevronRight } from "@blueprintjs/icons";
+import { json, useLoaderData } from "@remix-run/react";
+import { CloverClient } from "~/.server/CloverClient";
+
+export const loader = async () => {
+  const client = new CloverClient();
+  const satellites = await client.listSatellites();
+  return json({ satellites });
+};
+
 export default function Index() {
-  return <></>;
+  const { satellites } = useLoaderData<typeof loader>();
+
+  return (
+    <main className="container mx-auto space-y-6 py-8">
+      <Section title="Satellites">
+        <SectionCard padded={false}>
+          <CardList>
+            {satellites.map((satellite) => (
+              <Card key={satellite.id} interactive={true} className="relative">
+                <div className="flex w-full justify-between">
+                  <span>{satellite.name}</span>
+                  <ChevronRight />
+                </div>
+              </Card>
+            ))}
+          </CardList>
+        </SectionCard>
+      </Section>
+    </main>
+  );
 }
