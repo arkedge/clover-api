00b8452a1e08d1e7b582fb36eb7058eb9d0ce9cd
Takahiro Miyoshi <miyoshi-takahiro@arkedgespace.com>
2025-06-23 17:22:51 +0900

    Refactor eslint.config.js

--- client-ui/eslint.config.js
+++ client-ui/eslint.config.js
@@ -1,29 +1,30 @@
-import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier/flat";
 import importPlugin from "eslint-plugin-import";
 import jsxA11y from "eslint-plugin-jsx-a11y";
-import pluginReact from "eslint-plugin-react";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import { configs as tseslintConfigs } from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["build", "app/gen"]),
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-    plugins: { js },
-    extends: [
-      "js/recommended",
-      tseslintConfigs.recommended,
-      pluginReact.configs.flat.recommended,
-      pluginReact.configs.flat["jsx-runtime"],
-      reactHooks.configs["recommended-latest"],
-      jsxA11y.flatConfigs.recommended,
-      importPlugin.flatConfigs.recommended,
-      importPlugin.flatConfigs.typescript,
-    ],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  globalIgnores(["build/"]),
+  eslint.configs.recommended,
+
+  // TypeScript
+  tseslintConfigs.recommended,
+
+  // plugin-react
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  {
     settings: {
       react: {
         version: "detect",
@@ -33,6 +34,17 @@ export default defineConfig([
         { name: "Link", linkAttribute: "to" },
         { name: "NavLink", linkAttribute: "to" },
       ],
+    },
+  },
+
+  // plugin-react-hooks
+  reactHooks.configs["recommended-latest"],
+
+  // plugin-import
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  {
+    settings: {
       "import/resolver": {
         typescript: {
           alwaysTryTypes: true,
@@ -40,5 +52,18 @@ export default defineConfig([
       },
     },
   },
-  eslintConfigPrettier,
+
+  // plugin-jsx-a11y
+  jsxA11y.flatConfigs.recommended,
+
+  // config-prettier
+  prettier,
+
+  // app/gen
+  {
+    files: ["app/gen/**/*"],
+    linterOptions: {
+      reportUnusedDisableDirectives: "off",
+    },
+  },
 ]);
